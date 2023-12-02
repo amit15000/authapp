e00c94448743dab36669933e4a8cca78fc0118b2
Amit Kumar <15000amitkumar@gmail.com>
2023-12-03 02:31:47 +0530

    PORT setup

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 app.use(express.json());
 require("./config/database").connect();
 require("dotenv").config();
-
+PORT = process.env.PORT || 4000;
 //mount route
 const authRoutes = require("./routes/authRoutes.js");
 app.use("/api/vi", authRoutes);
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log("Server started successfully");
+app.listen(PORT, () => {
+  console.log(`Server started successfully on PORT : ${PORT}`);
 });
