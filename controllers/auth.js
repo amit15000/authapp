e4722a12f955cebaeb39808fca92e8d476f84e25
Amit Kumar <15000amitkumar@gmail.com>
2023-12-03 02:26:28 +0530

    Signup controller written

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const Person = require("../model/Person");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res) => {
   try {
@@ -10,12 +11,36 @@ exports.signup = async (req, res) => {
         sucess: false,
         message: "User already exist",
       });
-
-      //means user is new --> email checked--> password need to be secured
     }
+    //means user is new --> email checked--> password need to be secured
+    let hashedPassword;
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch {
+      return res.status(500).json({
+        sucess: false,
+        message: "Error in hashing password",
+      });
+    }
+    const person = await Person.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    res.status(200).json({
+      success: true,
+      message: "User created successfully",
+    });
   } catch {}
 };
 exports.login = async (req, res) => {
   try {
-  } catch {}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User can't be refist",
+    });
+  }
 };
