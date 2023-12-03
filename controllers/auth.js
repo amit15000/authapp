7936f99baa06f99592e15e0facb097d1bd9dee3a
Amit Kumar <15000amitkumar@gmail.com>
2023-12-03 18:46:08 +0530

    Login Page Created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const Person = require("../model/Person");
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 exports.signup = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
@@ -36,11 +37,72 @@ exports.signup = async (req, res) => {
 };
 exports.login = async (req, res) => {
   try {
+    const { email, password } = req.body;
+
+    //check if fields are filled
+
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "Fill all fields",
+      });
+    }
+    //check if email is present in db
+
+    const user = await Person.findOne({ email });
+
+    if (!user) {
+      //user has not yet signed up
+      return res.json({
+        success: false,
+        message: "SignUp First before login",
+      });
+    }
+
+    //check his password
+
+    //payload
+    const payload = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+
+    if (await bcrypt.compare(password, user.password)) {
+      //password matched
+      //create a token
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      //we have create cokkie now
+
+      //add new token elemen to the user object
+      user.token = token;
+      //remove password from this object
+      user.password = undefined;
+
+      const options = {
+        expiresIn: new Date(Date.now + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+      return res.cookie("AmitCookie", token, options).json({
+        sucess: true,
+        token: token,
+        user: user,
+        message: "User Logged in Successfully",
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: "Password not matched",
+      });
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
-      message: "User can't be refist",
+      message: "Login failed",
     });
   }
 };
